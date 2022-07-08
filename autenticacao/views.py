059c51c9aeb7712fda6afe11354dbc9631ed5b06
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-07-08 17:11:57 -0300

    criado pagina de cadastro

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-    return HttpResponse('Você está na página de cadastro')
+    return render(request, 'cadastro.html')
+
+
+def logar(request):
+    return HttpResponse('Você está na página de login')
