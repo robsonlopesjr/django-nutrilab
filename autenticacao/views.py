17e48b59cce6f73a8f4cb00b8e9364ca056ac5ce
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-07-08 14:55:51 -0300

    inserindo app de autenticacao

---
+++ autenticacao/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def cadastro(request):
+    return HttpResponse('Você está na página de cadastro')
