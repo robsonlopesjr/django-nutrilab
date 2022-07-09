d5e41ff543d0eb6b5e00b8c2a67812484ce18dd8
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2022-07-09 09:08:35 -0300

    inserindo validação de senhas

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .utils import password_is_valid
 
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+
+        return HttpResponse(f'O usuario {usuario} tem o email {email}')
 
 
 def logar(request):
